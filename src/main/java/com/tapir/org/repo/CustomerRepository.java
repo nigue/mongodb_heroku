f063be5ae673e3c253c889f9e15025bf91ba3fb7
mfuenzalida <mfuenzalida@modyo.com>
2019-01-22 14:34:34 -0300

    first commit

---
+++ src/main/java/com/tapir/org/repo/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.tapir.org.repo;
+
+import com.tapir.org.entity.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+}
